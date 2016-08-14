c009c8cd46ae7c93b8bc1120940f859382fb1f9e
Zoë Sparks <zoe@milky.flowers>
2016-08-14 19:24:50 -0400

    Change from chatterbot to twitter
    The updates to chatterbot made auth setup on the server impractical.
    Change the tweet and reply scripts to use the twitter gem instead.

--- reply.rb
+++ reply.rb
@@ -1,7 +1,39 @@
+require 'psych'
+require 'twitter'
 require 'require_all'
 require_rel 'app'
-require 'chatterbot/dsl'
 
-replies do |tweet|
-  reply "#USER# " + ReplyCombinator.new.generate, tweet
+auth_file = File.open('./auth.yml') {|f| f.read }
+auth = Psych.load auth_file
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = auth[:consumer_key]
+  config.consumer_secret     = auth[:consumer_secret]
+  config.access_token        = auth[:token]
+  config.access_token_secret = auth[:secret]
+end
+
+last_replied_mention_id = File.open('./last_replied_mention.txt') {|f| f.read}.to_i
+
+def gen_reply(sn_to_reply)
+  reply_content = ReplyCombinator.new.generate
+  reply = "@#{sn_to_reply} #{reply_content}"
+  checker = CharChecker.new
+  unless checker.check_char_count(reply)
+    gen_reply(screen_name)
+  end
+  reply
+end
+
+mentions_needing_replies = client.mentions_timeline(since_id: last_replied_mention_id)
+mentions_needing_replies.each do |tweet|
+  sn_to_reply = tweet.user.screen_name
+  reply = gen_reply(sn_to_reply)
+  client.update(reply, in_reply_to_status: tweet)
+end
+
+most_recent_replied_to_id = mentions_needing_replies.first.id
+
+File.open('./last_replied_mention.txt', 'w') do |f| 
+  f.puts most_recent_replied_to_id
 end
--- tweet.rb
+++ tweet.rb
@@ -1,5 +1,16 @@
+require 'psych'
+require 'twitter'
 require 'require_all'
 require_rel 'app'
-require 'chatterbot/dsl'
 
-tweet MasterCombinator.new.generate
+auth_file = File.open('./auth.yml') {|f| f.read }
+auth = Psych.load auth_file
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = auth[:consumer_key]
+  config.consumer_secret     = auth[:consumer_secret]
+  config.access_token        = auth[:token]
+  config.access_token_secret = auth[:secret]
+end
+
+client.update MasterCombinator.new.generate
